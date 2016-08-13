bb04a45d5896433de70bb85bd380dd15ce6e3dc0
ischung <insang@hansung.ac.kr>
2016-08-13 18:29:25 +0900

    add dec method in Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -2,11 +2,15 @@ package sig.oss;
 
 public class Calc 
 {
+	public int r=0;
 	public int add(int x, int y) { //add method
 		return x+y;
 	}
 	public int sub(int x, int y) { //add method
 		return x-y;
+	}
+	public void dec(int d) { //add method
+		r-=d;
 	}
 
 }
